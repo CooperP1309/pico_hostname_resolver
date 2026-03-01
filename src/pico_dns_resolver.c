4a9439e5db0d0ba545ecb04a5897da2afbfa253b
CooperP1309 <cooperpeek1309@gmail.com>
2026-03-02 10:46:28 +1100

    Implemented async resolving

--- src/pico_dns_resolver.c
+++ src/pico_dns_resolver.c
@@ -6,25 +6,31 @@
 #include "wl_log.h"
 
 static void dns_found_cb(const char *name, const ip_addr_t *ipaddr, void *callback_arg) {
-    if (ipaddr) {
+    
+    ip_addr_t *passed_ip = (ip_addr_t *)callback_arg;
+
+    if (ipaddr && passed_ip) {
+        *passed_ip = *ipaddr; // copy resolved IP to caller's variable
         WL_LOGI("DNS", "Hostname '%s' resolved to IP: %s", name, ipaddr_ntoa(ipaddr));
     } else {
         WL_LOGE("DNS", "Failed to resolve hostname '%s'", name);
     }
 }
 
-void resolve_hostname(const char *hostname) {
+void resolve_hostname(const char *hostname, ip_addr_t *addr) {
 
     ip_addr_t dns0 = *dns_getserver(0);
     WL_LOGI("NET", "DNS0: %s", ipaddr_ntoa(&dns0));
 
-    ip_addr_t addr; // we can pass NULL here since we're using a callback
-
+    // dns_gethostbyname() must be wrapped wrapped with lwip's begin and end calls
+    // to ensure thread safety when using lwip in a background thread
     cyw43_arch_lwip_begin();
 
+    // dns_gethostbyname() (name to resolve, addr to store for immmadiate results,
+    //                       callback for async results, callback arg)
+    // Callback arg is where the address will be stored if results are async
+    err_t err = dns_gethostbyname(hostname, addr, dns_found_cb, addr);
     
-
-    err_t err = dns_gethostbyname(hostname, &addr, dns_found_cb, NULL);
     cyw43_arch_lwip_end();
 
     if (err == ERR_OK) {
